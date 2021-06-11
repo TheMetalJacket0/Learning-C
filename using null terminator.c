09caefd9270665759abb7936cf7293c9b51f80ce
TheMetalJacket0 <25673887+TheMetalJacket0@users.noreply.github.com>
2021-06-10 22:31:16 -0400

    Create  using null terminator.c

---
+++ using null terminator.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(void){
+    char first[101],last[101];
+    int i,num;
+    scanf("%d",&num);
+    
+    for(i=0;i<num;i++){
+        
+        scanf("%s %s",first,last);
+        printf("%s %s\n",last, first);
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
