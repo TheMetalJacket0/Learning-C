2f86cb4b8ac9674bdb469f064e49c9198f4946cc
TheMetalJacket0 <25673887+TheMetalJacket0@users.noreply.github.com>
2021-06-10 22:30:51 -0400

    Create even or odd number of letters in a word.c

---
+++ even or odd number of letters in a word.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main (void){
+    
+    char name[51];
+    int i;
+    i=0;
+    
+    
+    scanf("%s",name);
+    
+    while(name[i]!='\0'){
+        i++;
+        
+        
+        
+        
+    }
+    if(i%2==0){
+        
+        printf("1");
+        
+        
+        
+    }else{
+        
+        printf("2");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
